7f1fe7fe8ec314cca843fd0e6dbec98e4f7dfa27
liubinbin <lotosbin@gmail.com>
2015-03-07 14:35:58 +0800

    readme

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ smsbao
 ======
 
 smsbao c# sdk
+
+# 安装 Install
+PM> Install-Package smsbao-dotnet-sdk
+
+# 配置 Config
+  <appSettings>
+    <add key="smsbao_username" value="username"/>
+    <add key="smsbao_password" value="password"/>
+  </appSettings>
+
+# 使用 Usage
+var client = new SmsBaoClient();
+client.SendSms("[phonenumber]", "[message]");
+
+# 问答 Q&A
+- 替换签名
+    前面添加自己的签名即可。
+
---
+++ smsbao.test/ISmsBaoClientTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace smsbao.test
+{
+    [TestClass]
+    public class ISmsBaoClientTest
+    {
+        [TestMethod]
+        public void SendSmsTest()
+        {
+            var client = new SmsBaoClient();
+            var mobile = ConfigurationManager.AppSettings["mobile"];
+            var result = client.SendSms(mobile, "【神猪】sldkfslkdjl");
+            Debug.WriteLine(JsonConvert.SerializeObject(result));
+            Assert.IsTrue(result.Success);
+        }
+    }
+}
